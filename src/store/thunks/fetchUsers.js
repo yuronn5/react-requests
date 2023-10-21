19d42e90f9d1adc7cd765757268880b991bf196c
yurii bilous <yurii.b@elogic.com.ua>
2023-10-21 13:31:45 +0300

    to test out loading process lifehack function

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -5,7 +5,17 @@ const fetchUsers = createAsyncThunk('users/fetch', async () => {
     const response = await axios.get('http://localhost:3005/users');
     //response.data === [{id: 1, name: 'Myra'}] get all of names list;
 
+    //dev only
+    await pause(1000);
+
     return response.data;
 });
+
+//dev only!!!
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration)
+    })
+}
 
 export { fetchUsers };
