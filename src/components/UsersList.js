412ea76ef2ffda0ca4a62182625150fa40e4b094
yurii bilous <yurii.b@elogic.com.ua>
2023-10-21 14:02:49 +0300

    anumation skeleton component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
+import Skeleton from "./Skeleton";
 
 function UsersList() {
     const dispatch = useDispatch();
@@ -13,7 +14,7 @@ function UsersList() {
     }, [dispatch]);
 
     if (isLoading) {
-        return <div>loading...</div>;
+        return <Skeleton times={6} className="h-10 w-full" />;
     }
 
     if (error) {
