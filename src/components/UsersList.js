f87d3a9dd3fca6207cb9e613115d00c8fc876378
yurii bilous <yurii.b@elogic.com.ua>
2023-10-22 14:22:21 +0300

    add user functionality

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers } from '../store';
+import { fetchUsers, addUser } from '../store';
 import Skeleton from "./Skeleton";
+import Button from "./Button";
 
 function UsersList() {
     const dispatch = useDispatch();
@@ -12,6 +13,10 @@ function UsersList() {
     useEffect(() => {
         dispatch(fetchUsers());
     }, [dispatch]);
+
+    const handleUserAdd = () => {
+        dispatch(addUser())
+    }
 
     if (isLoading) {
         return <Skeleton times={6} className="h-10 w-full" />;
@@ -31,7 +36,15 @@ function UsersList() {
         );
     });
 
-    return <div>{renderedUsers}</div>;
+    return <div>
+        <div className="flex flex-row justify-between m3">
+            <h1 className="m-2 text-xl">Users</h1>
+            <Button onClick={handleUserAdd}>
+                + Add User
+            </Button>
+        </div>
+        {renderedUsers}
+    </div>;
 }
 
 export default UsersList;
