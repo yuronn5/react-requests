991c7df47ddf0345e87dda7394d4db312aa13042
yurii bilous <yurii.b@elogic.com.ua>
2023-10-24 21:15:48 +0300

    button component add uploader icon when you click add user (spiner icon)

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -43,9 +43,7 @@ function UsersList() {
     return <div>
         <div className="flex flex-row justify-between m3">
             <h1 className="m-2 text-xl">Users</h1>
-            {isCreatingUser ? 'Creating user ...' :
-                <Button onClick={handleUserAdd}>+ Add User</Button>
-            }
+            <Button loading={isCreatingUser} onClick={handleUserAdd}>+ Add User</Button>
             {creatingUserError && 'error creating user...'}
         </div>
         {renderedUsers}
