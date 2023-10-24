3777069dc03b72f87db16e1d8fdec093f4f07b7c
yurii bilous <yurii.b@elogic.com.ua>
2023-10-24 21:02:11 +0300

    creating custom hook(reusable thunk hook) to manage previous code (also refactored it)

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,39 +1,25 @@
-import {useEffect, useState} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import {useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { fetchUsers, addUser } from '../store';
 import Skeleton from "./Skeleton";
 import Button from "./Button";
 import {fa} from "@faker-js/faker";
+import { useThunk } from "../hooks/use-thunk";
 
 function UsersList() {
-    const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-    const [loadingUsersError, setLoadingUsersError] = useState(null);
-    const [isCreatingUser, setIsCreatingUser] = useState(false);
-    const [creatingUserError, setCreatingUserError] = useState(null);
-    const dispatch = useDispatch();
+    const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+    const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
     const { data } = useSelector((state) => {
         return state.users;
     });
 
     useEffect(() => {
-        setIsLoadingUsers(true);
-        dispatch(fetchUsers())
-            .unwrap()
-            .catch((err) => {
-                console.log("request fail")
-                setLoadingUsersError(err);
-            })
-            .finally(() => {
-                setIsLoadingUsers(false)
-            })
-    }, [dispatch]);
+        doFetchUsers();
+    }, [doFetchUsers]);
 
     const handleUserAdd = () => {
-        setIsCreatingUser(true);
-        dispatch(addUser())
-            .unwrap()
-            .catch(err => setCreatingUserError(err))
-            .finally(() => setIsCreatingUser(false))
+        doCreateUser();
     }
 
     if (isLoadingUsers) {
