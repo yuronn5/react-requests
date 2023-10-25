113a73a273ba940fb082e8b89c6e5143470b0bf2
yurii bilous <yurii.b@elogic.com.ua>
2023-10-25 14:04:30 +0300

    collapsible expand panel

--- src/components/ExpandablePanel.js
+++ src/components/ExpandablePanel.js
@@ -1,14 +1,26 @@
+import { useState} from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
 function ExpandablePanel({header, children}) {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleClick = () => {
+        setExpanded(!expanded)
+    }
+
     return (
         <div className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
+            <div className="flex p-2 justify-between items-center">
                 <div className="flex flex-row items-center justify-between gap-3">
                     {header}
                 </div>
+                <div onClick={handleClick} className="cursor-pointer">
+                    {expanded ? <GoChevronDown/> : <GoChevronLeft/>}
+                </div>
             </div>
-            <div className="p-2 border-t">
-                {children}
-            </div>
+            {
+                expanded && <div className="p-2 border-t">{children}</div>
+            }
         </div>
     );
 }
