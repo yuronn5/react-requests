7e49c502aaa64d1c73056d81b19f9f705e8b8335
yurii bilous <yurii.b@elogic.com.ua>
2023-10-25 13:33:31 +0300

    Users 1 item component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -5,6 +5,7 @@ import Skeleton from "./Skeleton";
 import Button from "./Button";
 import {fa} from "@faker-js/faker";
 import { useThunk } from "../hooks/use-thunk";
+import UsersListItem from "./UsersListItem";
 
 function UsersList() {
     const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
@@ -29,13 +30,7 @@ function UsersList() {
         content = <div>Error fetching data...</div>;
     } else {
         content = data.map((user) => {
-            return (
-                <div key={user.id} className="mb-2 border rounded">
-                    <div className="flex p-2 justify-between items-center cursor-pointer">
-                        {user.name}
-                    </div>
-                </div>
-            );
+            return <UsersListItem key={user.id} user={user}/>
         });
     }
 
