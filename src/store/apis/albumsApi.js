63363757c3f3a9dc5434d5e2a7703627efe4312c
yurii bilous <yurii.b@elogic.com.ua>
2023-11-04 14:53:11 +0200

    rtk styling

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,10 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import {faker} from "@faker-js/faker";
 
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration)
+    })
+}
+
 const albumsApi = createApi({
     reducerPath: 'albums',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3005'
+        baseUrl: 'http://localhost:3005',
+        fetchFn: async (...args) => {
+            //remove for prod
+            await pause(1000);
+            return fetch(...args);
+        }
     }),
     endpoints(builder) {
         return {
