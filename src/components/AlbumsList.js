0ab05915d0b9c4182903aeaa0632ab28cb918454
yurii bilous <yurii.b@elogic.com.ua>
2023-11-04 13:36:51 +0200

    rtk functionality for get albums

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,6 +1,35 @@
+import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+import {el} from "@faker-js/faker";
+
 function AlbumsList({user}) {
+    const {data, error, isLoading} = useFetchAlbumsQuery(user);
+    // console.log(data,error,isLoading)
+
+    let content;
+    if (isLoading) {
+        content = <Skeleton times={3}/>
+    } else if (error) {
+        content = <div>Error loading albums</div>
+    } else {
+        content = data.map(album => {
+            const header = <div>{album.title}</div>
+            return <ExpandablePanel key={album.id} header={header}>
+                List of photo in the album
+            </ExpandablePanel>
+        })
+    }
+
+
     return <div>
-        Albums for {user.name}
+        <div>
+            Albums for {user.name}
+        </div>
+        <div>
+            {content}
+        </div>
     </div>
 }
 
